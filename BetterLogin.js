1d84df73d5ddd26d1b1af96367688960526cc5a2
qwertyp0 <logan.liao97@gmail.com>
2018-04-07 11:46:47 -0400

    new login page

---
+++ BetterLogin.js
@@ -0,0 +1,42 @@
+/*$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});*/
+function setCookie(cname, cvalue, exdays) {
+    "use strict";
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    return true;
+}
+    
+    
+function setAllCookie() {
+     "use strict";
+    if (!setCookie("user-name", document.getElementById("username").value, 1)){
+        return false;
+    }
+    if (!setCookie("pass-word", document.getElementById("password").value, 1)){
+        return false;
+    }
+    alert(getCookie("user-name"));
+    window.location.href = "";
+    /**put in dashboard here*/
+}
+    
+function getCookie(cname) {
+     "use strict";
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
